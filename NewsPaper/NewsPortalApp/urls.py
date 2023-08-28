262656e82397cf33e36bc19262ec5c41f52c1b6e
maatvej <136988224+maatvej@users.noreply.github.com>
2023-08-28 14:55:50 +0300

    Add files via upload

---
+++ NewsPaper/NewsPortalApp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import PostsList, PostDetail, PostSearch, NewsCreate, ArticleCreate, PostUpdate,PostDelete
+
+urlpatterns = [
+    path('', PostsList.as_view(), name='post_list'),
+    path('<int:pk>', PostDetail.as_view(), name='post_detail'),
+    path('search', PostSearch.as_view(), name='post_search'),
+    path('postnews', NewsCreate.as_view(), name='news_create'),
+    path('postarticle', ArticleCreate.as_view(), name='article_create'),
+    path('<int:pk>/update/', PostUpdate.as_view(), name='post_update'),
+    path('<int:pk>/delete/', PostDelete.as_view(), name='post_delete'),
+]
---
+++ NewsPaper/NewsPortalApp/views.py
@@ -0,0 +1,87 @@
+# Импортируем класс, который говорит нам о том,
+# что в этом представлении мы будем выводить список объектов из БД
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from .filters import PostFilter
+from .forms import PostForm
+
+
+class PostsList(ListView):
+    model = Post
+    ordering = '-postingDate'
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+    paginate_by = 10
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
+
+
+class PostSearch(ListView):
+    model = Post
+    ordering = '-postingDate'
+    template_name = 'search.html'
+    context_object_name = 'postsearch'
+    paginate_by = 10
+
+    def get_queryset(self):
+        # Получаем обычный запрос
+        queryset = super().get_queryset()
+        # Используем наш класс фильтрации.
+        # self.request.GET содержит объект QueryDict, который мы рассматривали
+        # в этом юните ранее.
+        # Сохраняем нашу фильтрацию в объекте класса,
+        # чтобы потом добавить в контекст и использовать в шаблоне.
+        self.filterset = PostFilter(self.request.GET, queryset)
+        # Возвращаем из функции отфильтрованный список товаров
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Добавляем в контекст объект фильтрации.
+        context['filterset'] = self.filterset
+        return context
+
+
+class NewsCreate(CreateView):
+    # Указываем нашу разработанную форму
+    form_class = PostForm
+    # модель товаров
+    model = Post
+    # и новый шаблон, в котором используется форма.
+    template_name = 'post_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.options = 'N'
+        return super().form_valid(form)
+
+
+class ArticleCreate(CreateView):
+    # Указываем нашу разработанную форму
+    form_class = PostForm
+    # модель товаров
+    model = Post
+    # и новый шаблон, в котором используется форма.
+    template_name = 'post_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.options = 'A'
+        return super().form_valid(form)
+
+
+class PostUpdate(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list')
